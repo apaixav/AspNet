26f3c1e95668fa26410a38657622106701be420c
apaixav <lucasrib01@gmail.com>
2023-09-21 17:04:42 -0300

    HelloWorld

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+
+
+    [Route("~/")]
+    [ApiController]
+    public class HelloController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello turma .NET 03";
+        }
+
+        [HttpGet("/meunome")]
+        public string MeuNome()
+        {
+            return "Lucas";
+        }
+    }
+}
